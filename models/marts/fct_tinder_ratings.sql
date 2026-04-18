baa75455729093232e91d67ffdd5b1e1232dca88
Phil Howson <philjhowson@outlook.com>
2026-04-18 12:58:16 +0000

    updating tinder ratings

--- models/marts/fct_tinder_ratings.sql
+++ models/marts/fct_tinder_ratings.sql
@@ -1,7 +1,20 @@
-select date,
-  country,
-  device,
-  rating_type,
-  value
-from {{ref('stg_ratings')}}
-where app = '{{var("target_app")}}'
+with totals as (
+    select date,
+      country,
+      device, 
+      value as total_ratings
+    from {{ref('stg_ratings')}}
+    where rating_type = 'rating_total'
+)
+
+select r.date,
+  r.country,
+  r.device,
+  r.rating_type,
+  r.value,
+  case when r.rating_type != 'avg_rating' then r.value / t.total_ratings else NULL end as pct_ratings
+  from {{ref('stg_ratings')}} r
+join totals t
+  using (date, country, device)
+where r.rating_type != 'total_rating'
+    and r.app = '{{var("target_app")}}'
