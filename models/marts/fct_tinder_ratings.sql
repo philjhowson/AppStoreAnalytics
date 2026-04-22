20767276bff545a20c05bd20f74545f376f00c7f
philjhowson <43015032+philjhowson@users.noreply.github.com>
2026-04-22 13:28:36 +0200

    Update fct_tinder_ratings.sql
    updating sql

--- models/marts/fct_tinder_ratings.sql
+++ models/marts/fct_tinder_ratings.sql
@@ -1,22 +1,17 @@
-with totals as (
-    select date,
-      country,
-      device, 
-      sum(value) as total_ratings
-    from {{ref('stg_ratings')}}
-    where rating_type != 'rating_total'
-        and app = '{{var("target_app")}}'
-    group by date, country, device
-)
+select
+    date,
+    country,
+    device,
+    rating_type,
+    value,
 
-select r.date,
-  r.country,
-  r.device,
-  r.rating_type,
-  r.value,
-  case when r.rating_type != 'avg_rating' then r.value / t.total_ratings else NULL end as pct_ratings
-  from {{ref('stg_ratings')}} r
-join totals t
-  using (date, country, device)
-where r.rating_type != 'rating_total'
-    and r.app = '{{var("target_app")}}'
+    case 
+        when rating_type != 'avg_rating'
+        then value / SUM(value) OVER (
+            PARTITION BY date, country, device
+        )
+    end as pct_ratings
+
+from {{ref('stg_ratings')}}
+where rating_type != 'rating_total'
+  and app = '{{var("target_app")}}'
