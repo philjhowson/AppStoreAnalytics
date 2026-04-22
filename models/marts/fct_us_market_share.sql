3916e56bc8f707bbc35805cf0e7a89f916aa4734
philjhowson <philjhowson@outlook.com>
2026-04-23 00:02:08 +0200

    updating image

--- models/marts/fct_us_market_share.sql
+++ models/marts/fct_us_market_share.sql
@@ -1,25 +1,14 @@
-with category_totals as (
-  select category,
-    date,
-    device,
-    metric,
-    value
-  from {{ref('stg_categories')}}
-where country = '{{var("target_country")}}'
-    and (
-      (device = 'android' and category = 'dating')
-      or
-      (device = 'iphone' and category = 'lifestyle')
-    )
-)
-
-select m.app,
-  m.date,
-  m.device,
-  m.metric,
-  m.value,
-  round(m.value / c.value, 4) as pct_total
-from {{ref('stg_metrics')}} m
-join category_totals c
-  using(date, device, metric)
+select
+  app,
+  date,
+  device,
+  metric,
+  value,
+  round(
+    value / sum(value) over (
+      partition by date, device, metric
+    ),
+    4
+  ) as pct_total
+from {{ref('stg_metrics')}}
 where country = '{{var("target_country")}}'
